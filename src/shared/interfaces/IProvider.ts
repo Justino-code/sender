3d474ccfdf8cb9ee39b60cccb4491ca466657c55
Justino Contingo <protectedum@proton.me>
2026-09-24 16:57:55 +0100

    refactor: mover interfaces e erros do shared para o core
    move as interfaces de providers de shared para core
    
    move as definições de erros para core
    
    remove os arquivos que não pertenciam à responsabilidade de shared
    
    ajusta a estrutura do projeto para refletir melhor as responsabilidades do core

--- src/shared/interfaces/IProvider.ts
+++
@@ -1,6 +0,0 @@
-import { SendBatchMessageDto, SendBatchMessageResponse, SendMessageDto, SendMessageResponse } from "../types/index.js";
-
-export interface IProvider {
-  send(data: SendMessageDto): Promise<SendMessageResponse>;
-  sendBatch(data: SendBatchMessageDto): Promise<SendBatchMessageResponse>;
-}
